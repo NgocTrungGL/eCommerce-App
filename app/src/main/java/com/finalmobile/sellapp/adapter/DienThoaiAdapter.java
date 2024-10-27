3c89234192aa555c6c62062f6f7113dc384efac8
No Pain No Gain <ngoctrung.it22@gmail.com>
2024-10-27 16:55:57 +0700

    Delete app directory

--- app/src/main/java/com/finalmobile/sellapp/adapter/DienThoaiAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.finalmobile.sellapp.adapter;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.> {
-}
--- app/src/main/java/com/finalmobile/sellapp/utils/Utils.java
+++
@@ -1,5 +0,0 @@
-package com.finalmobile.sellapp.utils;
-
-public class Utils {
-    public static final String BASE_URL = "http://192.168.1.10/banhang/";
-}
