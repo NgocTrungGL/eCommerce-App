41321dc03623a62cc8baa9356665415e488e4608
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-10-28 23:23:47 +0700

    done HomePage And Login&SignUp

---
+++ lib/views/categories_screen/category_screen.dart
@@ -0,0 +1,11 @@
+import 'package:emart_app/widgets_common/bg_widget.dart';
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return bgWidget();
+  }
+}
---
+++ lib/widgets_common/our_button.dart
@@ -0,0 +1,17 @@
+import 'package:emart_app/consts/consts.dart';
+import 'package:flutter/material.dart';
+
+Widget ourButton({onPress, color, textColor, String? title}) {
+  return ElevatedButton(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: color,
+      padding: const EdgeInsets.all(12),
+    ),
+    onPressed: onPress,
+    child: title!.text.color(textColor).fontFamily(bold).make(),
+    // style: ElevatedButton.styleFrom(
+    //   primary: redColor,
+    //   padding: const EdgeInsets.all(12),
+    // ),
+  );
+}
